c6e9f837eccc5824daa9ea324f313497c030cab0
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2014-09-01 11:31:04 +0000

    Allow freeradius::site to accept 'content' as well as 'source'

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,14 @@
 # Install FreeRADIUS virtual servers (sites)
-define freeradius::site ($source) {
+define freeradius::site (
+  $source  = undef,
+  $content = undef,
+) {
   file { "/etc/raddb/sites-enabled/${name}":
     mode    => '0640',
     owner   => 'root',
     group   => 'radiusd',
     source  => $source,
+    content => $content,
     require => Package['freeradius'],
     notify  => Service['radiusd'],
   }
