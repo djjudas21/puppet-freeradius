05facb403b141ea0333a6d1c69d1574bf2a4c6c5
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2014-09-01 14:11:35 +0000

    Take out automatic firewall management (for now). Add enable/disable param

--- manifests/status_server.pp
+++ manifests/status_server.pp
@@ -1,21 +1,13 @@
 class freeradius::status_server (
   $port     = '18121',
   $listen   = '*',
-  $ipaddr   = '127.0.0.1',
   $secret,
-  $firewall = false,
+  $enable   = true,
 ) {
   freeradius::site { 'status':
-    content => template('freeradius/sites-enabled/status.erb'),
-#    source => 'puppet:///modules/freeradius/sites-enabled/status',
-  }
-
-  if $firewall == true {
-    firewall { '100-radius-status':
-      proto  => 'udp',
-      dport  => $port,
-      source => $ipaddr,
-      action => 'accept',
+    content => $enable ? {
+      true    => template('freeradius/sites-enabled/status.erb'),
+      default => '# Status server disabled',
     }
   }
 }
