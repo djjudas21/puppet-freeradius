97af2a54a683806241f45a39119ce431a64c962c
Matt Wise <matt@nextdoor.com>
2015-01-21 13:27:54 -0800

    Do not show_diff in freeradius::cert.
    This can lead to private keys being exposed in logs when Puppet runs.

--- manifests/cert.pp
+++ manifests/cert.pp
@@ -17,13 +17,14 @@ define freeradius::cert (
   }
 
   file { "${fr_basepath}/certs/${name}":
-    ensure   => $ensure,
-    mode     => $permission,
-    owner    => 'root',
-    group    => $fr_group,
-    source   => $source,
-    content  => $content,
-    require  => [File["${fr_basepath}/certs"], Package[$fr_package], Group[$fr_group]],
-    notify   => Service[$fr_service],
+    ensure    => $ensure,
+    mode      => $permission,
+    owner     => 'root',
+    group     => $fr_group,
+    source    => $source,
+    content   => $content,
+    show_diff => false,
+    require   => [File["${fr_basepath}/certs"], Package[$fr_package], Group[$fr_group]],
+    notify    => Service[$fr_service],
   }
 }
