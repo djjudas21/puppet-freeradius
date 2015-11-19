7e33cb49b193edbb1af9f583d4c34f8a001e70e1
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-11-19 16:32:58 +0000

    Remove dodgy conditionals so the facts evaluate properly

--- lib/facter/freeradius_version.rb
+++ lib/facter/freeradius_version.rb
@@ -1,9 +1,6 @@
 # Grab the FreeRADIUS version from the output of radiusd -v
-if %x{which radiusd 2>/dev/null | wc -l}.chomp.to_i > 0
-  Facter.add(:freeradius_version) do
-    setcode do
-      Facter::Core::Execution.exec('radiusd -v').split(/\n/)[0].match(/FreeRADIUS Version (\d\.\d\.\d)/)[1]
-    end
+Facter.add(:freeradius_version) do
+  setcode do
+    Facter::Core::Execution.exec('radiusd -v').split(/\n/)[0].match(/FreeRADIUS Version (\d\.\d\.\d)/)[1]
   end
 end
-
