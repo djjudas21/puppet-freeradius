d8a696e44834f52a2b96bd9b601f0f82b1483102
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-11-25 16:40:23 +0000

    Make facts fail gracefully if radiusd is not installed

--- lib/facter/freeradius_version.rb
+++ lib/facter/freeradius_version.rb
@@ -1,6 +1,11 @@
 # Grab the FreeRADIUS version from the output of radiusd -v
 Facter.add(:freeradius_version) do
   setcode do
-    Facter::Core::Execution.exec('radiusd -v').split(/\n/)[0].match(/FreeRADIUS Version (\d\.\d\.\d)/)[1]
+    version = Facter::Core::Execution.exec('radiusd -v')
+    if !version.nil?
+      version = version.split(/\n/)[0].match(/FreeRADIUS Version (\d\.\d\.\d)/)[1].to_s
+    end
+    version
   end
 end
+
